d14ee9b7bb027deee38b500db5e89d903c8388b1
3190104610@zju.edu.cn <3190104610@zju.edu.cn>
2021-12-04 15:04:26 +0800

    基本完成动态规划

--- main.py
+++ main.py
@@ -39,31 +39,68 @@ if __name__ == '__main__':
 
     time.sleep(0.1)  # 防止未连接上仿真环境
 
-    package = Debug_Msgs()
+    start = (2400, 1500)
+    end = (-2400, -1500)
+    
+    for i in range(10):
+        if i % 2 == 0:
+            dst = end
+        else:
+            dst = start
 
-    planner = APF(vision)
-    planner.set_para(time_limit=200)
+        package = Debug_Msgs()
 
-    # 规划路径
-    waypoint_list = planner.plan(-2400, -1500)
-    draw_waypoint(waypoint_list, collision, debugger,
-                  package, good_way_color=Debug_Msg.YELLOW)
+        planner = APF(vision)
+        planner.set_para(time_limit=200)
 
-    # 化简路径
-    waypoint_list = simplifier.simplify(waypoint_list)
-    draw_waypoint(waypoint_list, collision, debugger, package)
+        # 规划路径
+        waypoint_list = planner.plan(dst[0], dst[1])
+        draw_waypoint(waypoint_list, collision, debugger,
+                    package, good_way_color=Debug_Msg.YELLOW)
 
-    debugger.draw_circle(package, 2400, 1500, 100)  # 绘制起点
-    debugger.draw_circle(package, -2400, -1500, 100)  # 绘制终点
+        # 化简路径
+        waypoint_list = simplifier.simplify(waypoint_list)
+        draw_waypoint(waypoint_list, collision, debugger, package)
 
-    debugger.send(package)
+        debugger.draw_circle(package, 2400, 1500, 100)  # 绘制起点
+        debugger.draw_circle(package, -2400, -1500, 100)  # 绘制终点
 
-    # 轨迹规划
-    move = Move(vision, waypoint_list)
-    while True:
-        vx, vw, flag = move.move_plan()
-        if flag:
-            action.sendCommand(vx=0, vy=0, vw=0)
-            break
-        action.sendCommand(vx=vx, vy=0, vw=vw)
-        time.sleep(0.02)
+        debugger.send(package)
+
+        # 轨迹规划
+        move = Move(vision, waypoint_list, 0, 0)
+        action.sendCommand(vx=0, vy=0, vw=0)
+
+        
+        flag = 0
+        while flag == 0:
+            cur_location = move.get_location()
+            is_collided = collision.is_collided_check_all(waypoint_list[cur_location][0],
+                                                        waypoint_list[cur_location][1],
+                                                        waypoint_list[cur_location+1][0],
+                                                        waypoint_list[cur_location+1][1])
+            if is_collided == True:
+                cur_vx, cur_vw = move.get_speed()
+                planner = APF(vision)
+                planner.set_para(time_limit=200)
+                package = Debug_Msgs()
+
+                waypoint_list = planner.plan(dst[0], dst[1])
+                draw_waypoint(waypoint_list, collision, debugger,
+                            package, good_way_color=Debug_Msg.YELLOW)
+
+                # 化简路径
+                waypoint_list = simplifier.simplify(waypoint_list)
+                draw_waypoint(waypoint_list, collision, debugger, package)
+
+                move = Move(vision, waypoint_list, cur_vx, cur_vw)
+
+                debugger.draw_circle(package, 2400, 1500, 100)  # 绘制起点
+                debugger.draw_circle(package, -2400, -1500, 100)  # 绘制终点
+
+                debugger.send(package)
+            
+            vx, vw, flag = move.move_plan()
+            action.sendCommand(vx=vx, vy=0, vw=vw)
+            time.sleep(0.01)
+
--- move.py
+++ move.py
@@ -1,26 +1,49 @@
 import math
-
+from datetime import datetime
 from vision import Vision
 
 
+MAX_VX = 3000
+MAX_AX = 3000
+MAX_VW = 5
+MAX_AW = 5
+ACC = 0
+DEC = 1
+
+
 class Move:
-    def __init__(self, vision: Vision, waypoint: list) -> None:
+    def __init__(self, vision: Vision, waypoint: list, vx, vw) -> None:
         self.vision = vision
         self.my_robot = self.vision.my_robot
 
-        self.cur_x = self.vision.my_robot.x
-        self.cur_y = self.vision.my_robot.y
-        self.orientation = self.vision.my_robot.orientation
+        self.cur_x = self.my_robot.x
+        self.cur_y = self.my_robot.y
+        self.orientation = self.my_robot.orientation
+        self.cur_vx = vx
+        self.cur_vw = vw
+
         self.waypoint = waypoint
         self.num_waypoint = len(waypoint)
         self.cur_waypoint = 0
-        self.k1 = 3
-        self.k2 = 3
+
+        self.cur_point_angle = self._turn_angle(self.cur_waypoint)
+        self.next_point_angle = self._turn_angle(self.cur_waypoint+1)
+        self.cur_point_speed = self._turn_speed(self.cur_point_angle)
+        self.next_point_speed = self._turn_speed(self.next_point_angle)
+        self.is_angle_update = False
+
+        self.k1 = 2
+        self.k21 = 3
+        self.k22 = 0.5
+        
+        self.cur_time = datetime.now()
+
+        self.cur_status = ACC
 
     def move_plan(self) -> tuple:
         flag = 0
-        self.cur_x = self.vision.my_robot.x
-        self.cur_y = self.vision.my_robot.y
+        self.cur_x = self.my_robot.x
+        self.cur_y = self.my_robot.y
 
         next_distance = self._point_distance(self.cur_x, self.cur_y,
                                              self.waypoint[self.cur_waypoint + 1][0],
@@ -37,6 +60,12 @@ class Move:
                 flag = 1
                 return vx, vw, flag
             else:
+                self.cur_point_angle = self._turn_angle(self.cur_waypoint)
+                self.next_point_angle = self._turn_angle(self.cur_waypoint+1)
+                self.cur_point_speed = self._turn_speed(self.cur_point_angle)
+                self.next_point_speed = self._turn_speed(self.next_point_angle)
+                self.cur_status = ACC
+                
                 next_distance = self._point_distance(self.cur_x, self.cur_y,
                                                      self.waypoint[self.cur_waypoint + 1][0],
                                                      self.waypoint[self.cur_waypoint + 1][1])
@@ -44,13 +73,14 @@ class Move:
                 pre_distance = self._point_distance(self.cur_x, self.cur_y,
                                                     self.waypoint[self.cur_waypoint][0],
                                                     self.waypoint[self.cur_waypoint][1])
+                
 
         way_distance = self._point_distance(self.waypoint[self.cur_waypoint][0],
                                             self.waypoint[self.cur_waypoint][1],
                                             self.waypoint[self.cur_waypoint + 1][0],
                                             self.waypoint[self.cur_waypoint + 1][1])
 
-        theta = self.orientation = self.vision.my_robot.orientation
+        theta = self.orientation = self.my_robot.orientation
         beta = self._point_arccos(self.cur_x, self.cur_y,
                                   self.waypoint[self.cur_waypoint+1][0],
                                   self.waypoint[self.cur_waypoint+1][1])
@@ -58,23 +88,58 @@ class Move:
         if self.orientation < 0:
             theta = 2 * math.pi + self.orientation
 
-        alpha = beta - theta
+        alpha = beta - theta 
 
         if alpha < - math.pi:
             alpha = 2 * math.pi + alpha
         elif alpha > math.pi:
             alpha = alpha - 2 * math.pi
+        
+        delta_t = (datetime.now() - self.cur_time).microseconds * 0.000001
+        self.cur_time = datetime.now()
 
-        if pre_distance < (way_distance / 5):
-            vx = self.k1 * pre_distance + 50
-        elif next_distance < (way_distance / 5):
-            vx = self.k1 * next_distance + 50
-        else:
-            vx = self.k1 * way_distance / 5 + 50
+        # print(self.cur_vx)
+        # print(pre_distance,next_distance)
+        # print(self.cur_point_angle, self.next_point_angle)
 
-        vw = self.k2 * alpha
-        print(vx, vw)
+        # vx规划
+        if self.cur_status == ACC:
+            if self.cur_vx - self.next_point_speed >= self.k1 * next_distance:
+                self.cur_status = DEC
+                vx = self.next_point_speed + self.k1 * next_distance
+            else:
+                vx = self.cur_point_speed + self.k1 * pre_distance
+        elif self.cur_status == DEC:
+            vx = self.next_point_speed + self.k1 * next_distance
 
+        # print(self.cur_status)
+        # print('------------------------')
+        
+        # 限制vx范围
+        vx = self.limit_vx_for_alpha(vx, alpha)
+        vx_min,vx_max = self._vx_space(delta_t)
+        if vx < vx_min:
+            vx = vx_min
+        elif vx > vx_max:
+            vx = vx_max 
+        self.cur_vx = vx
+
+        # vw规划
+        if alpha < math.pi/2 and alpha > -math.pi/2:
+            vw = self.k21 * alpha
+        elif alpha > math.pi/2:
+            vw = self.k21 * math.pi/2 + self.k22 * (alpha - math.pi/2)
+        elif alpha < -math.pi/2:
+            vw = -self.k21 * math.pi/2 + self.k22 * (-alpha + math.pi/2)
+        
+        vw_min,vw_max = self._vw_space(delta_t)
+        if vw < vw_min:
+            vw = vw_min
+        elif vw > vw_max:
+            vw = vw_max 
+        self.cur_vw = vw
+
+        # print(self.cur_waypoint,self.next_point_angle,self.next_point_speed)
         return vx, vw, flag
 
     def _point_distance(self, x1: float, y1: float, x2: float, y2: float) -> float:
@@ -86,5 +151,70 @@ class Move:
         else:
             beta = 2 * math.pi - \
                 math.acos((x2-x1) / self._point_distance(x1, y1, x2, y2))
-
         return beta
+
+    def _turn_speed(self, alpha: float) -> float:
+        degree = abs(alpha / (2 * math.pi) * 360)
+        # if degree < 30:
+        #     turn_speed = 14/9 * pow((degree - 30), 2) + 600
+        # elif degree < 85:
+        #     turn_speed = -10 * degree + 900
+        if degree < 70:
+            turn_speed = -10 * degree + 750
+        else:
+            turn_speed = 50
+        return turn_speed
+
+    def _vx_space(self, delta_t: float) -> tuple:
+        vx_max = min(MAX_VX, self.cur_vx+delta_t*MAX_AX)
+        vx_min = max(0, self.cur_vx-delta_t*MAX_AX)
+        return vx_min, vx_max
+
+    def _vw_space(self, delta_t: float) -> tuple:
+        vw_max = min(MAX_VW, self.cur_vw+delta_t*MAX_AW)
+        vw_min = max(-MAX_VW, self.cur_vw-delta_t*MAX_AW)
+        return vw_min, vw_max
+    
+    def get_speed(self) -> tuple:
+        return self.cur_vx, self.cur_vw
+    
+    def get_location(self) -> int:
+        return self.cur_waypoint
+    
+    def _turn_angle(self, seq: int) -> float:
+        if seq == self.num_waypoint -1:
+            angle = 180
+            return angle
+
+        if seq == 0:
+            pre_angle = self.orientation
+            if pre_angle < 0:
+                pre_angle = 2 * math.pi + pre_angle
+        else:
+            pre_angle = self._point_arccos(self.waypoint[seq-1][0],
+                                           self.waypoint[seq-1][1],
+                                           self.waypoint[seq][0],
+                                           self.waypoint[seq][1])
+        
+        next_angle = self._point_arccos(self.waypoint[seq][0],
+                                        self.waypoint[seq][1],
+                                        self.waypoint[seq+1][0],
+                                        self.waypoint[seq+1][1])
+        angle = next_angle-pre_angle
+        if angle < - math.pi:
+            alpha = 2 * math.pi + angle
+        elif angle > math.pi:
+            alpha = angle - 2 * math.pi
+        return angle
+    
+    def limit_vx_for_alpha(self, vx0:float, alpha: float) -> float:
+        degree = abs(alpha / (2 * math.pi) * 360)
+        if degree >= 70:
+            vx = 0
+        elif degree >= 30:
+            vx = vx0 / 3
+        elif degree >= 10:
+            vx = vx0 / 2
+        else:
+            vx = vx0
+        return vx
